e0f177999e68ba656af37e3e319e4399d55579f6
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2023-08-14 14:59:46 -0400

    Add files via upload

--- voicetype.py
+++ voicetype.py
@@ -1,9 +1,10 @@
 import pyautogui as pr
 import speech_recognition as sr
+import os
 r=sr.Recognizer()
+os.system('espeak -ven-us "what do you want to type?')
 with sr.Microphone() as source:
     r.adjust_for_ambient_noise(source)
-    print('speak...')
     audio=r.listen(source)
     saidtxt=r.recognize_google(audio)
 pr.write(saidtxt)
