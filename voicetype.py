11644aeacc4fee5e296bf9312aa4169872161a2c
Mrpi314tech <111321481+Mrpi314tech@users.noreply.github.com>
2023-08-14 15:03:51 -0400

    Update voicetype.py

--- voicetype.py
+++ voicetype.py
@@ -2,7 +2,7 @@ import pyautogui as pr
 import speech_recognition as sr
 import os
 r=sr.Recognizer()
-os.system('espeak -ven-us "what do you want to type?')
+os.system('espeak -ven-us "what do you want to type?"')
 with sr.Microphone() as source:
     r.adjust_for_ambient_noise(source)
     audio=r.listen(source)
